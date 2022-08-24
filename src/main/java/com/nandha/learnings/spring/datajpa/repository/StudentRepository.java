6a2693d5e2566e72f272687347e937e548d46bfc
Nandha Kumar <48424140+nandha-jpg@users.noreply.github.com>
2022-08-24 20:58:59 +0530

    added JPQL query

--- src/main/java/com/nandha/learnings/spring/datajpa/repository/StudentRepository.java
+++ src/main/java/com/nandha/learnings/spring/datajpa/repository/StudentRepository.java
@@ -2,6 +2,7 @@ package com.nandha.learnings.spring.datajpa.repository;
 
 import com.nandha.learnings.spring.datajpa.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +13,8 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findStudentByFirstName(String firstName);
     List<Student> findStudentByLastName(String lastName);
     List<Student> findStudentByFirstNameContaining(String name);
+
+    //JPQL (This is defined based on the classes and attributes that you have created)
+    @Query("select s from Student s where s.firstName = ?1")
+    Student getStudentByFirstName(String firstName);
 }
