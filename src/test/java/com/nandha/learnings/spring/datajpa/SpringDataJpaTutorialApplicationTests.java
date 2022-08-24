7492b57e72eaaad91f773bc3c895fc8323ed934f
Nandha Kumar <48424140+nandha-jpg@users.noreply.github.com>
2022-08-24 14:40:35 +0530

    Initial commit

---
+++ src/test/java/com/nandha/learnings/spring/datajpa/SpringDataJpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nandha.learnings.spring.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
