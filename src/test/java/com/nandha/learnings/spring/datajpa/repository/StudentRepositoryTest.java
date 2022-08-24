ee328de78d0ef2eb0b809de764cd916209b03266
Nandha Kumar <48424140+nandha-jpg@users.noreply.github.com>
2022-08-24 15:51:47 +0530

    created test class

---
+++ src/test/java/com/nandha/learnings/spring/datajpa/repository/StudentRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.nandha.learnings.spring.datajpa.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@DataJpaTest /*This will help you to test the repository layer and once the operation is done, it will flush the data.
+//               So, the database won't be impacted.'*/
+@SpringBootTest //Ideally you should not use this annotation to test the repository layer. But currently, we want the database to be impacted
+class StudentRepositoryTest {
+
+}
