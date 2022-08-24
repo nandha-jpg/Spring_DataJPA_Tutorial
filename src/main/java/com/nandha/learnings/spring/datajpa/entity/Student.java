c4ee8f6bec6f7b672fbad540b53469bcc36f732b
Nandha Kumar <48424140+nandha-jpg@users.noreply.github.com>
2022-08-24 15:22:25 +0530

    different JPA methods

--- src/main/java/com/nandha/learnings/spring/datajpa/entity/Student.java
+++ src/main/java/com/nandha/learnings/spring/datajpa/entity/Student.java
@@ -11,13 +11,30 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "student_tbl",
+        uniqueConstraints = @UniqueConstraint(
+                name = "uc_email",
+                columnNames = {"email"}))
 public class Student {
     @Id
-    private Long studentId;
+    @Column(name = "student_id")
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence")
+    private Long id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "guardian_name")
     private String guardianName;
+    @Column(name = "email")
     private String guardianEmail;
+    @Column(name = "guardian_mobile")
     private String guardianMobile;
 
 }
