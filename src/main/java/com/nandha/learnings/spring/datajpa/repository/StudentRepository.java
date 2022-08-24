03cabb0faa2a4103e2880e1396b94325b8cd3600
Nandha Kumar <48424140+nandha-jpg@users.noreply.github.com>
2022-08-24 20:58:59 +0530

    added JPQL query

--- src/main/java/com/nandha/learnings/spring/datajpa/repository/StudentRepository.java
+++ src/main/java/com/nandha/learnings/spring/datajpa/repository/StudentRepository.java
@@ -17,17 +17,4 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     //JPQL (This is defined based on the classes and attributes that you have created)
     @Query("select s from Student s where s.firstName = ?1")
     Student getStudentByFirstName(String firstName);
-
-    @Query("select s.lastName from Student s where s.emailAddress = ?1")
-    String getStudentLastNameByEmail(String emailAddress);
-
-    //Native query
-    @Query(value = "select * from student_tbl where first_name = ?1",
-            nativeQuery = true)
-    Student getStudentByFirstNameNativeQuery(String firstName);
-
-    @Query(value = "select s.last_name from student_tbl s where s.email = ?1",
-            nativeQuery = true)
-    String getStudentLastNameByEmailNative(String email);
-
 }
