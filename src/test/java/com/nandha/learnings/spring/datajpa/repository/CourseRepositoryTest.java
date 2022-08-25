4e8f3fc7ecb34a32e9de8c0454adc6419da65672
Nandha Kumar <48424140+nandha-jpg@users.noreply.github.com>
2022-08-25 13:20:03 +0530

    find all courses using one to one unidirectional mapping

--- src/test/java/com/nandha/learnings/spring/datajpa/repository/CourseRepositoryTest.java
+++ src/test/java/com/nandha/learnings/spring/datajpa/repository/CourseRepositoryTest.java
@@ -1,7 +1,22 @@
 package com.nandha.learnings.spring.datajpa.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.nandha.learnings.spring.datajpa.entity.Course;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
 class CourseRepositoryTest {
 
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Test
+    public void findAllCourses(){
+        List<Course> courses = courseRepository.findAll();
+        System.out.println("courses = " + courses);
+    }
 }
